2f99dbe72ba08bf4085267e4e6a8e912af1aff0f
little-pinecone <marta.szymek.mail@gmail.com>
2022-03-16 14:56:44 +0100

    fix: add octet-stream to supported media types

--- src/main/java/in/keepgrowing/openapiclientcodegenerationtest/config/swagger/SwaggerBeanConfig.java
+++ src/main/java/in/keepgrowing/openapiclientcodegenerationtest/config/swagger/SwaggerBeanConfig.java
@@ -1,11 +1,10 @@
 package in.keepgrowing.openapiclientcodegenerationtest.config.swagger;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-import java.util.List;
+import java.util.ArrayList;
 
 @Configuration
 public class SwaggerBeanConfig {
@@ -14,12 +13,11 @@ public class SwaggerBeanConfig {
      * The reason for this class is to provide a custom HttpMessageConverter to deserialize
      * application/octet-stream bodies using Jackson.
      *
-     * @see <a href="https://github.com/swagger-api/swagger-ui/issues/6462#issuecomment-929189296">support custom content-type selection</a>
+     * @see <a href="https://github.com/swagger-api/swagger-ui/issues/6462">support custom content-type selection</a>
      */
-    @Bean
-    public MappingJackson2HttpMessageConverter octetStreamJsonConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(List.of(new MediaType("application", "octet-stream")));
-        return converter;
+    public SwaggerBeanConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
     }
 }
